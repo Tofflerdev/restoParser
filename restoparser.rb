7cc0c9622de6ecd96316404766410f7f4729fdea
TofflerDev <baraban.ms@gmail.com>
2019-04-02 00:38:56 +0300

    tested restoparser

--- restoparser.rb
+++ restoparser.rb
@@ -3,7 +3,7 @@ require 'watir'
 
 puts "Loading links"
 input = File.read('urls.csv')
-links = input.split("\n")
+links = input.split("\n").slice(52,99)
 puts "#{links.count} links loaded"
 
 browser = Watir::Browser.new :firefox
@@ -13,17 +13,18 @@ links.each do |link|
     browser.goto link
     browser.ul(class: "baseCategoriesUl").wait_until(&:present?)
 
-    name = browser.h1(class: "print").text
+    name = browser.h1(itemprop: "name").text
     adress = browser.h2(class: "sAddress").text
     website = browser.a(id: "companyUrl").text
-    email = browser.a(class: "companyMail").text
+    email = browser.a(class: "companyMail").text if browser.a(class: "companyMail").present?
     description = browser.p(class: "description").text
-    phones = browser.divs(class: "tel")
-    subcats = browser.lis(class: "category")
+    phones =  []
+    browser.divs(class: "tel").each{|tel| phones << tel.text}
+    subcats = browser.ul(class: "baseCategoriesUl").text.split("\n").join(",")
     
     puts "Writing to the file"
 
-    File.open('output.csv', 'a'){|file| file.write("#{name},#{adress},#{website},#{email},#{description}\n")}
+    File.open('output.csv', 'a'){|file| file.write("#{name};#{adress};#{website};#{email};#{description};#{phones.join(",")};#{subcats}\n")}
 
 
 end
